0d9ad4088bf704c5418f9599e011883b6f611b42
Alice Bizzarri <bzzlca@unife.it>
2026-09-10 13:34:55 +0200

    some fix

--- tensorflow_implementation/neural_network.py
+++ tensorflow_implementation/neural_network.py
@@ -277,8 +277,8 @@ class NeuralNetwork(BaseNeuralNetwork):
         self.model.optimizer = opt
 
         # --- Callbacks ---
-        es = EarlyStopping(monitor="val_loss", min_delta=0.005, patience=10, verbose=1,
-                            mode="min", restore_best_weights=True)
+        es = EarlyStopping(monitor="val_accuracy", min_delta=0.005, patience=50, verbose=1,
+                            mode="max", restore_best_weights=True)
         # --- Compile ---
         self.model.model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
         
